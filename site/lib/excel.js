7588948fef7c590cb41ec868cbf2a6222ae74f24
arynak24 <aryn.kimura@gmail.com>
2026-03-26 15:05:43 -0700

    Add functions to read and write Excel cell values

--- site/lib/excel.js
+++ site/lib/excel.js
@@ -1,1 +1,22 @@
+/**
+ * Read text from selected Excel cells.
+ */
+export async function getSelectedText() {
+    return Excel.run(async (context) => {
+        const range = context.workbook.getSelectedRange();
+        range.load("text");
+        await context.sync();
+        return range.text.join("\n");
+    });
+}
 
+/**
+ * Write value into selected Excel cell.
+ */
+export async function writeToSelectedCell(value) {
+    return Excel.run(async (context) => {
+        const cell = context.workbook.getSelectedRange();
+        cell.values = [[value]];
+        await context.sync();
+    });
+}
